076718a8299e90384fe7ddcb3560da0bb63d215f
phpBBeesti <info@phpbbeesti.com>
2015-05-29 18:01:42 +0300

    Create movemessage.php

---
+++ language/et/movemessage.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Move Message [Estonian]
+*
+* @package phpBB.de Move Message
+* @copyright (c) 2015 phpBB.de; Estonian translation by phpBBeesti.com 05/2015
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+$lang = array_merge($lang, array(
+	'MOVED_MESSAGE'				=> 'See postitus on liigutatud <strong>%1$s</strong> foorumist <strong>%2$s</strong> foorumisse, %3$s liikme <strong>%4$s</strong> poolt',
+));
