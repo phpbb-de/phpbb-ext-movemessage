7c8ee8e66aa557ba0f5353777f5a3c8bc2d6f20d
Raphaël M. <Galixte@users.noreply.github.com>
2015-07-05 18:52:34 +0200

    FRENCH translation for Move Message extension

---
+++ language/fr/movemessage.php
@@ -0,0 +1,43 @@
+<?php
+/**
+*
+* Move Message extension for the phpBB Forum Software package.
+* French translation by Galixte (http://www.galixte.com)
+*
+* @copyright (c) 2015 phpBB.de
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
+$lang = array_merge($lang, array(
+	'MOVED_MESSAGE'				=> 'Message déplacé du forum <strong>%1$s</strong> vers le forum <strong>%2$s</strong> par %3$s le <strong>%4$s</strong>',
+));
