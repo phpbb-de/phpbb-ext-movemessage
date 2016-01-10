e662d2507aa078498e207c656a136abe76ccb46f
phpBB España <thekuka@gmail.com>
2016-01-10 12:46:40 +0100

    Spanish translation
    Spanish translation

---
+++ language/es/movemessage.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* Move Message [Spanish]
+*
+* @package phpBB.de Move Message
+* @copyright (c) 2015 phpBB.de
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'MOVED_MESSAGE'				=> 'Movido desde <strong>%1$s</strong> a <strong>%2$s</strong> el %3$s por <strong>%4$s</strong>',
+));
